d00e5f41ff0880d1b9be3a41aa3ce909012b12a7
Dharshini310 <dharshini3102k3@gmail.com>
2024-05-20 12:02:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Tictactoe from './Components/Tic Tac Toe'
+
+function App() {
+
+  return (
+    <div>
+      <Tictactoe/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/GameState.jsx
@@ -0,0 +1,7 @@
+const GameState ={
+    playerXwins : 0,
+    payerOwins : 1,
+    draw : 2,
+    inProgress:3,
+};
+export default GameState;
